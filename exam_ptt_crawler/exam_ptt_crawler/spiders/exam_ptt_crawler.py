b1ef2378cf07f354916beedaf89f9b34d5887c24
ZenoHTY <44594821+zenohsutingyi@users.noreply.github.com>
2018-10-31 12:26:45 +0800

    Part2 PTT crawler
    Not finished yet

---
+++ exam_ptt_crawler/exam_ptt_crawler/spiders/exam_ptt_crawler.py
@@ -0,0 +1,47 @@
+import datetime
+import scrapy
+
+from exam_ptt_crawler.items import PostItem
+
+class PTTSpider(scrapy.Spider):
+    # ...
+    def parse(self, response):
+        item = PostItem()
+        item['title'] = response.xpath(
+            '//meta[@property="og:title"]/@content')[0].extract()
+        item['author'] = response.xpath(
+            '//div[@class="article-metaline"]/span[text()="作者"]/following-sibling::span[1]/text()')[
+                0].extract().split(' ')[0]
+        datetime_str = response.xpath(
+            '//div[@class="article-metaline"]/span[text()="時間"]/following-sibling::span[1]/text()')[
+                0].extract()
+        item['date'] = datetime.strptime(datetime_str, '%a %b %d %H:%M:%S %Y')
+
+        item['content'] = response.xpath('//div[@id="main-content"]/text()')[
+            0].extract()
+
+        comments = []
+        total_score = 0
+        for comment in response.xpath('//div[@class="push"]'):
+            push_tag = comment.css('span.push-tag::text')[0].extract()
+            push_user = comment.css('span.push-userid::text')[0].extract()
+            push_content = comment.css('span.push-content::text')[0].extract()
+
+            if '推' in push_tag:
+                score = 1
+            elif '噓' in push_tag:
+                score = -1
+            else:
+                score = 0
+
+            total_score += score
+
+            comments.append({'user': push_user,
+                             'content': push_content,
+                             'score': score})
+
+        item['comments'] = comments
+        item['score'] = total_score
+        item['url'] = response.url
+
+        yield item
